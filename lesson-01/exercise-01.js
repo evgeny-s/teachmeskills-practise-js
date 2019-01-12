afdf6c2d0207371c352254083f691325a9e8792b
Evgeny Svirsky <evgeny@funler.com>
2019-01-13 02:24:04 +0300

    Text fixes.

--- lesson-01/exercise-01.js
+++ lesson-01/exercise-01.js
@@ -1,2 +1,5 @@
-// Write a JavaScript program to convert a given string into an array of words.
-// interface: stringToArray(someString)
+// Exercise 01: Write a JavaScript program to convert a given string into an array of words.
+
+const stringToArray = (str) => {
+  // Solution here...
+};
