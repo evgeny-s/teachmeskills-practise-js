1afff3d012b9cc77a1428002f472cff1d17d2575
Evgeny Svirsky <evgeny@funler.com>
2019-01-17 16:49:02 +0300

    Added homework.

--- lesson-02/homework.js
+++ lesson-02/homework.js
@@ -1,1 +1,8 @@
-//TODO: Create homework...
+//Homework: Need to create an application in order to check what Goods can buy Users with Bank's help. Steps:
+// 1. Need to create 4 classes: App, User, Good, Bank.
+// 2. User will have properties: firstName, lastName, income
+// 3. Good will have properties: name, price
+// 4. Bank will have properties: loanPercentagePerYear
+// 5. Bank should have method: checkIfUserCanByGood(user, good). This method should check if user can take a loan for buying specific good. It is depends on his income. If user can pay for the loan in 5 years, bank will definitely give him a loan, otherwise - not.
+// 6. App should have method run(). This method should have 2 methods: generateUsers() and generateGoods(). Generate 10 random users and 10 random goods.
+// 7. App should have another method: startCheck(). This method will start the check procedure: check if every user can take a loan in order to purchase each good.
