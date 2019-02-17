4a135f862a827609d7f00da7897cc2f3228554f7
Evgeny Svirsky <evgeny@funler.com>
2019-02-17 13:41:12 +0300

    Added lesson 10, lesson 11 and graduation project.

---
+++ Graduation project/index.js
@@ -0,0 +1,4 @@
+// Graduation project description:
+// Clone initial repo: git@github.com:evgeny-s/tms-graduation-project.git
+// Pick any task here: https://docs.google.com/document/d/1bG07pdyXQ34d7lm--iKmWM65UY5_6fuQOwdK2HTGH9Y/edit?usp=sharin
+// Add a comment with your name to (Taken by: ***) section. So, nobody else will work on this task.
